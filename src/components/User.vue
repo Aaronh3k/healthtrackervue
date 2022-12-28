954de72d38b4cb281bc73e7464a606b1eceaa72e
aaron <20100312@mail.wit.ie>
2022-12-28 10:10:27 +0000

    added admin dashboard UI

---
+++ src/components/User.vue
@@ -0,0 +1,80 @@
+<template>
+  id: {{ $route.params.id }}
+  <v-form v-model="valid">
+    <v-container>
+      <v-row>
+        <v-col
+            cols="12"
+            md="4"
+        >
+          <v-text-field
+              v-model="firstname"
+              :rules="nameRules"
+              :counter="10"
+              label="First name id: "
+              required
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+            cols="12"
+            md="4"
+        >
+          <v-text-field
+              v-model="lastname"
+              :rules="nameRules"
+              :counter="10"
+              label="Last name"
+              required
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+            cols="12"
+            md="4"
+        >
+          <v-text-field
+              v-model="email"
+              :rules="emailRules"
+              label="E-mail"
+              required
+          ></v-text-field>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+import { useRoute } from 'vue-router'
+
+export default {
+  name: 'User',
+  data: () => ({
+    valid: false,
+    firstname: '',
+    lastname: '',
+    nameRules: [
+      v => !!v || 'Name is required',
+      v => v.length <= 10 || 'Name must be less than 10 characters',
+    ],
+    email: '',
+    emailRules: [
+      v => !!v || 'E-mail is required',
+      v => /.+@.+/.test(v) || 'E-mail must be valid',
+    ],
+  }),
+  computed(){
+    console.log($route.params.id)
+  }
+}
+</script>
+
+
+<!--<template>-->
+<!--  id: {{ $route.params.id }}-->
+<!--</template>-->
+
+<!--<script>-->
+<!--return this.$route.params.id-->
+<!--</script>-->
--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from "./router";
+import store from "./store";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FontAwesomeIcon } from './plugins/font-awesome'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
 
-createApp(App).mount('#app')
+loadFonts()
+
+createApp(App)
+    .use(vuetify)
+    .use(router)
+    .use(store)
+    .component("font-awesome-icon", FontAwesomeIcon)
+  .mount('#app')
+
